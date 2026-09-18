98c3d760aa46d91120eff719d6ebbad66080c702
Gabriel Alexandro Kaligis <2581026@unai.edu>
2026-09-18 09:57:16 +0700

    Memperbaiki Input

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -4,8 +4,6 @@ public class KartuMahasiswa {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-    System.out.print("Nama lengkap     : ");
-    String namaLengkap = input.nextLine();
 
     System.out.print("NIK (16 digit)   : ");
     long nik = input.nextLong();
@@ -25,6 +23,8 @@ public class KartuMahasiswa {
 
     input.nextLine();
 
+    System.out.print("Nama lengkap     : ");
+    String namaLengkap = input.nextLine();
 
     System.out.println();
     System.out.println("===== KARTU MAHASISWA =====");
