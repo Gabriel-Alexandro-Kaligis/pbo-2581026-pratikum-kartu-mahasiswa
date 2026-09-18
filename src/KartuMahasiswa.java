0b9ee1dcff47f6f754538559590807899d01442e
Gabriel Alexandro Kaligis <2581026@unai.edu>
2026-09-18 09:51:05 +0700

    Memperbaiki masalah output yang berantakan, dan Update Menambahkan Nama Lengkap, NIK, Umur,Tinggi, Gol Darah, dan Status Menikah

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -4,8 +4,36 @@ public class KartuMahasiswa {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
+    System.out.print("NIK (16 digit)   : ");
+    long nik = input.nextLong();
+
+    System.out.print("Umur             : ");
+    int umur = input.nextInt();
+
+    System.out.print("Tinggi badan cm  : ");
+    double tinggiBadan = input.nextDouble();
+
+    System.out.print("Golongan darah   : ");
+    char golonganDarah = input.next().charAt(0);
+
+    System.out.print("Sudah menikah    : ");
+    boolean sudahMenikah = input.nextBoolean();
+
+
+    input.nextLine();
 
     System.out.print("Nama lengkap     : ");
     String namaLengkap = input.nextLine();
 
+    System.out.println();
+    System.out.println("===== KARTU MAHASISWA =====");
     System.out.println("Nama       : " + namaLengkap);
+    System.out.println("NIK        : " + nik);
+    System.out.println("Umur       : " + umur + " tahun");
+    System.out.println("Tinggi     : " + tinggiBadan + " cm");
+    System.out.println("Gol darah  : " + golonganDarah);
+    System.out.println("Menikah    : " + sudahMenikah);
+
+    input.close();
+  }
+}
